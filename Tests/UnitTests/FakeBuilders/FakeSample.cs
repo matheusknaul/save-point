a1c378b25f6f6dac7ed44c6e4e49fe3cbfb03de2
matheusknaul <contatomatheush0909@gmail.com>
2025-09-27 22:58:09 -0300

    feat: create FakeSample

---
+++ Tests/UnitTests/FakeBuilders/FakeSample.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using UnitTests.Entities;
+
+namespace UnitTests.FakeBuilders
+{
+    public static class FakeSample
+    {
+        public static Sample Create()
+        {
+            var sampleFaker = new Faker<Sample>()
+                .RuleFor(s => s.Name, f => f.Lorem.Word())
+                .RuleFor(s => s.Description, f => f.Lorem.Sentence());
+            return sampleFaker.Generate();
+        }
+    }
+}
